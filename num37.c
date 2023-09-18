c39f3ed6240b1771cdccf31134e6d71d2c6371f5
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-09-18 21:01:40 +0700

    Update num37.c

--- num37.c
+++ num37.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main() {
     float a = 0;
     float b = 1;
     float x;
-    while (b - a > 0.00001){
+    while (b - a > 0.00001) {
         x = (b + a) / 2;
-        if (((pow(a, 4) + 2 * pow(a, 3) - a - 1) < (pow(b, 4) + 2 * pow(b, 3) - b - 1)) && (pow(x, 4) + 2 * pow(x, 3) - x - 1) >= 0)
+        if (((pow(a, 4) + 2 * pow(a, 3) - a - 1) < (pow(b, 4) + 2 * pow(b, 3) - b - 1)) &&
+            (pow(x, 4) + 2 * pow(x, 3) - x - 1) >= 0)
             b = x;
         else
             a = x;
-
-        printf("%f %f %f",a, b, x);
-        printf("\n");
     }
+    printf("%f", x);
+
+    return 0;
 }
