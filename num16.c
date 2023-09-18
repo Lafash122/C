fe4d2ebab3b0645cefbae0746d060366b7545cb0
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-09-18 18:59:48 +0700

    num16.c

--- num16.c
+++ num16.c
@@ -5,7 +5,9 @@ int main(){
     scanf("%d %d", &h, &m);
     cw = h * 5 + 15 + (h * 5 + 15) / 60 * 5;
     ccw = h * 5 - 15 + (60 * ((h * 5 - 15) <= 0));
-    min = ((((ccw - m) < (cw - m)) && ((ccw - m) >= 0)) * (ccw - m)) + ((cw - m) * (!(((ccw - m) < (cw - m)) && ((ccw - m) >= 0)) && (cw - m >=0))) + (ccw - m) * (cw - m < 0);
+    min = ((((ccw - m) < (cw - m)) && ((ccw - m) >= 0)) * (ccw - m))
+        + ((cw - m) * (!(((ccw - m) < (cw - m)) && ((ccw - m) >= 0)) && (cw - m >=0)))
+        + ((ccw - m) * (cw - m < 0)) + ((((h + 1) * 5 + 15) + (60 - ccw)) * ((ccw - m) < 0));
     printf("%d",min);
 
     return 0;
