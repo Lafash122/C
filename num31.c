d98b5ced39b0c2629c50e80373411d2fb2e4fb20
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-09-18 21:07:21 +0700

    Update num31.c

--- num31.c
+++ num31.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int c = 20 * 20 - 19 * 19;
     int b = 18;
-    while (b > 0){
+    while (b > 0) {
         c = c - b * b;
         b--;
     }
