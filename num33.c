066b44dd0962fe24f6c1ff2741ce9787709df1cf
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-09-18 21:06:15 +0700

    Update num33.c

--- num33.c
+++ num33.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main() {
     int x, n;
     int i = 1;
     float a = 1;
     float b = 1;
     scanf("%d %d", &x, &n);
-    while (i <= n){
+    while (i <= n) {
         b = b * i;
-        a = a + (float)(pow(-1, i) * (pow(x, i) / b));
+        a = a + (float) (pow(-1, i) * (pow(x, i) / b));
         i++;
     }
     printf("%f", a);
