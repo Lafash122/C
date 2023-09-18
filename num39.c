2f0bbc04f39f67ae791c7d4eedfcddd72a154f39
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-09-18 21:00:29 +0700

    Update num39.c

--- num39.c
+++ num39.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main() {
     float p0 = 1.29;
-    float z = 1.25 * pow(10,-4);
+    float z = 1.25 * pow(10, -4);
     int h = 0;
     float p = p0 * pow(M_E, h * z * (-1));
-    while (p >= 1){
+    while (p >= 1) {
         h++;
         p = p0 * pow(M_E, h * z * (-1));
     }
