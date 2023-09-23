d670511a594c984b1854928e6054b87e0f69d850
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-09-23 20:04:12 +0700

    num6.c

--- part2/num6.c
+++ part2/num6.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-//The recursion function that allow to search the sum of the digits of the number
-int sumel(int num) {
+int sumel(int num) {                            //The recursion function that allow to search the sum of the digits of the number
     if (num <= 9)
         return num;
     else
