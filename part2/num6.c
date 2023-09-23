9438c387db996e41953f7b6bdf556c27ad76c36e
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-09-23 16:50:09 +0700

    Add files via upload

---
+++ part2/num6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//The recursion function that allow to search
+int sumel(int num) {
+    if (num == 0) return 0;
+    else return sumel(num / 10) + (num % 10);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", sumel(n));
+
+    return 0;
+}
