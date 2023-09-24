a7962749cf80c652438de9562d760be3fde2292b
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-09-24 13:47:33 +0700

    num11.c

--- part2/num_11.c
+++ part2/num_11.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <malloc.h>
 
-void delrep(int *arr, int *len) {                                //The function that delete repeats from array
+void delrep(int *arr, int *len) {                                    //The function that delete repeats from array
     for (int i = 0; i < *len - 1; i++) {
-        for (int j = i + 1; j < *len; j ++) {
-            if (*(arr + i) == *(arr + j)){
+        for (int j = i + 1; j < *len; j++) {
+            if (*(arr + i) == *(arr + j)) {
                 *(arr + j) = *(arr + *len - 1);
                 *len = *len - 1;
                 j = j - 1;
-                arr = (int*) realloc(arr, *len * sizeof(int));
+                arr = (int *) realloc(arr, *len * sizeof(int));
 
             }
         }
@@ -19,7 +19,7 @@ int main() {
     int n, i;
     scanf("%d", &n);
     int *a;
-    a = (int*) malloc(n * sizeof(int));
+    a = (int *) malloc(n * sizeof(int));
     for (i = 0; i < n; i++)
         scanf("%d", a + i);
 
