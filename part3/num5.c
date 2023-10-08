e9a692fe3dc5a773704c337e98a8649747a65cf8
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-10-08 12:46:40 +0700

    num5.c

--- part3/num5.c
+++ part3/num5.c
@@ -2,49 +2,35 @@
 #include <string.h>
 #include <malloc.h>
 
-//The function that delete repeats from string
-int delrep(char *str, int *len) {
-    for (int i = 0; i < *len - 1; i++)
-        for (int j = i + 1; j < *len; j++)
-            if (str[i] == str[j]) {
-                str[j] = str[*len - 1];
-                *len = *len - 1;
-                j = j - 1;
-            }
-    return *len;
-}
-
 int main() {
     char sent1[80] = "", sent2[80] = "", sent3[80] = "";
-    int *arr;
-    int i, len, len1, len2, len3;
+    int *arr1, *arr2, *arr3;
+    int i;
     fgets(sent1, 80, stdin);
     fgets(sent2, 80, stdin);
     fgets(sent3, 80, stdin);
-    arr = (int *) malloc(128 * sizeof(int));
+    arr1 = (int *) malloc(128 * sizeof(int));
+    arr2 = (int *) malloc(128 * sizeof(int));
+    arr3 = (int *) malloc(128 * sizeof(int));
     for (i = 0; i < 128; i++)
-        arr[i] = 0;
+        arr1[i] = 0, arr2[i] = 0, arr3[i] = 0;
 
-    len = (strlen(sent1)) - 1;
-    len1 = delrep(sent1, &len);
-    for (i = 0; i < len1; i++)
-        arr[(int) sent1[i]]++;
+    for (i = 0; i < (strlen(sent1)); i++)
+        arr1[(int) sent1[i]] = 1;
 
-    len = (strlen(sent2)) - 1;
-    len2 = delrep(sent2, &len);
-    for (i = 0; i < len2; i++)
-        arr[(int) sent2[i]]++;
+    for (i = 0; i < (strlen(sent2)); i++)
+        arr2[(int) sent2[i]] = 1;
 
-    len = (strlen(sent3)) - 1;
-    len3 = delrep(sent3, &len);
-    for (i = 0; i < len3; i++)
-        arr[(int) sent3[i]]++;
+    for (i = 0; i < (strlen(sent3)); i++)
+        arr3[(int) sent3[i]] = 1;
 
     for (i = 0; i < 128; i++)
-        if (arr[i] == 1)
+        if (arr1[i] + arr2[i] + arr3[i] == 1)
             printf("%c ", i);
 
-    free(arr);
+    free(arr1);
+    free(arr2);
+    free(arr3);
 
     return 0;
 }
