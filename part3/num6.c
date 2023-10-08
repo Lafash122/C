01c0eff5cfe428e00e8025b97a28cca80005687b
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-10-08 18:09:19 +0700

    num6.c

--- part3/num6.c
+++ part3/num6.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <maloc.h>
 
 int main() {
     char sent[80] = "";
     fgets(sent, 80, stdin);
-    char seps[] = " ,:!()?.;'-";
-    int i, j, end;
-    end = strlen(sent) - 1;
-    for (i = end; i >= 0; i--) {
-        if (strchr(seps, sent[i]) != NULL) {
-            for (j = i + 1; j < end; j++)
-                printf("%c", sent[j]);
-            end = i;
-            printf(" ");
-        }
-        else if (i == 0)
-            for (j = i; j < end; j++)
-                printf("%c", sent[j]);
-
+    char seps[] = " ,:!()?.;'-\n";
+    char *token;
+    char **tokens;
+    int i = 0;
+    tokens = (char **) malloc(40 * sizeof(char *));
+    token = strtok(sent, seps);
+    while (token != NULL) {
+        tokens[i] = token;
+        token = strtok(NULL, seps);
+        i++;
     }
+
+    for (i; i >= 0; i--)
+        if (tokens[i] != NULL)
+            printf("%s ", tokens[i]);
 
     return 0;
 }
