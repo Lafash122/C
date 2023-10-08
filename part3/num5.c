5aa9c83d40e0cdf125a0a1943f934e5dc94d7cae
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-10-08 12:58:02 +0700

    num5.c

--- part3/num5.c
+++ part3/num5.c
@@ -3,26 +3,26 @@
 #include <malloc.h>
 
 int main() {
-    char sent1[80] = "", sent2[80] = "", sent3[80] = "";
+    char str1[80] = "", str2[80] = "", str3[80] = "";
     int *arr1, *arr2, *arr3;
     int i;
-    fgets(sent1, 80, stdin);
-    fgets(sent2, 80, stdin);
-    fgets(sent3, 80, stdin);
+    fgets(str1, 80, stdin);
+    fgets(str2, 80, stdin);
+    fgets(str3, 80, stdin);
     arr1 = (int *) malloc(128 * sizeof(int));
     arr2 = (int *) malloc(128 * sizeof(int));
     arr3 = (int *) malloc(128 * sizeof(int));
     for (i = 0; i < 128; i++)
         arr1[i] = 0, arr2[i] = 0, arr3[i] = 0;
 
-    for (i = 0; i < (strlen(sent1)); i++)
-        arr1[(int) sent1[i]] = 1;
+    for (i = 0; i < (strlen(str1)); i++)
+        arr1[(int) str1[i]] = 1;
 
-    for (i = 0; i < (strlen(sent2)); i++)
-        arr2[(int) sent2[i]] = 1;
+    for (i = 0; i < (strlen(str2)); i++)
+        arr2[(int) str2[i]] = 1;
 
-    for (i = 0; i < (strlen(sent3)); i++)
-        arr3[(int) sent3[i]] = 1;
+    for (i = 0; i < (strlen(str3)); i++)
+        arr3[(int) str3[i]] = 1;
 
     for (i = 0; i < 128; i++)
         if (arr1[i] + arr2[i] + arr3[i] == 1)
