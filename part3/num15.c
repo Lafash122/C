5c3cc50ba12377a16013cc27776486292a0f63d7
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2023-10-10 00:07:35 +0700

    Add files via upload

---
+++ part3/num15.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    char num[6] = "";
+    fgets(num, 6, stdin);
+    int i, j, k;
+    for (i = 0; i < 25; i++)
+        puts("");
+
+    for (i = 0; i < 10; i++) {
+        int bulls = 0, cows = 0;
+        char answer[6] = "";
+        fgets(answer, 6, stdin);
+        for (j = 0; j < 4; j++) {
+            if (answer[j] == num[j])
+                bulls++;
+            else
+                for (k = 0; k < 4; k++)
+                    if (answer[k] == num[j] && k != j)
+                        cows++;
+        }
+        printf("Bulls: %d, Cows: %d.\n", bulls, cows);
+        if (bulls == 4) {
+            puts("\nYou win!");
+            return 0;
+        }
+    }
+    puts("\nYou lose");
+    return 0;
+}
